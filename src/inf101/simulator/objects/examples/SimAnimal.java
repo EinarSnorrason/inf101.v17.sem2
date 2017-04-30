95062a7a5746f707191d3b76d039797d573ade87
Einar Snorrason <Einar.Snorrason@student.uib.no>
2017-04-30 15:56:52 +0200

    Endringer

--- src/inf101/simulator/objects/examples/SimAnimal.java
+++ src/inf101/simulator/objects/examples/SimAnimal.java
@@ -1,4 +1,5 @@
 package inf101.simulator.objects.examples;
+
 import inf101.simulator.Direction;
 import inf101.simulator.GraphicsHelper;
 import inf101.simulator.Habitat;
@@ -15,11 +16,12 @@ import javafx.scene.paint.Color;
 
 public class SimAnimal extends AbstractMovingObject {
 	private static final double defaultSpeed = 1.0;
-	private static final double VIEW_ANGLE = 90;
+	private static final double VIEW_ANGLE = 210;
 	private static final double VIEW_DISTANCE = 400;
 	private Habitat habitat;
 	private Image img;
-	
+	private Direction opposite;
+
 	/**
 	 * Tracks energy of SimAnimal.
 	 */
@@ -30,41 +32,47 @@ public class SimAnimal extends AbstractMovingObject {
 		this.habitat = hab;
 		img = MediaHelper.getImage("pipp.png");
 	}
-	
 
 	@Override
 	public void draw(GraphicsContext context) {
-		
+
 		super.draw(context);
+		
+		if (opposite != null){
+			Position oppositePos = getPosition().move(opposite, 100);
+			context.setStroke(Color.YELLOW);
+			context.strokeLine(0, 0, oppositePos.getX()-getX(), oppositePos.getY()-getY());
+			
+		}
 		
 		// Draw viewing angle
 		context.setStroke(Color.GREEN.deriveColor(0.0, 1.0, 1.0, 0.5));
-		GraphicsHelper.strokeArcAt(context, getWidth()/2, getHeight()/2, VIEW_DISTANCE, 0, VIEW_ANGLE);
+		GraphicsHelper.strokeArcAt(context, getWidth() / 2, getHeight() / 2, VIEW_DISTANCE, 0, VIEW_ANGLE);
 		// Draws the image (flips it if it would be upside-down)
 		double angle = getDirection().toAngle();
-		if (angle <90 && angle >-90){
+		if (angle < 90 && angle > -90) {
 			context.scale(1, -1);
 			context.translate(0, -getHeight());
 		}
-		context.drawImage(img,0,0,getWidth(),getHeight());
+		context.drawImage(img, 0, 0, getWidth(), getHeight());
 	}
 
 	public IEdibleObject getBestFood() {
 		return getClosestFood();
 	}
-	
+
 	/**
 	 * Gives the closest food object in the animal's view cone
+	 * 
 	 * @return
 	 */
 	public IEdibleObject getClosestFood() {
-		for (ISimObject obj : habitat.nearbyObjects(this, getRadius()+VIEW_DISTANCE)) {
-			if(obj instanceof IEdibleObject && canSee(obj, VIEW_ANGLE) &&
-					habitat.contains(obj.getPosition())){
+		for (ISimObject obj : habitat.nearbyObjects(this, getRadius() + VIEW_DISTANCE)) {
+			if (obj instanceof IEdibleObject && canSee(obj, VIEW_ANGLE) && habitat.contains(obj.getPosition())) {
 				return (IEdibleObject) obj;
 			}
 		}
-		
+
 		return null;
 	}
 
@@ -77,7 +85,34 @@ public class SimAnimal extends AbstractMovingObject {
 	public double getWidth() {
 		return 50;
 	}
-	
+
+	private void goToFood() {
+		// If you find food, go towards it
+		IEdibleObject food = getBestFood();
+
+		if (food != null) {
+			dir = dir.turnTowards(directionTo(food), 2);
+			if (distanceToTouch(food) <= 0) {
+				// Stop moving when eating
+				energy += food.eat(0.5);
+				accelerateTo(0, 0.1);
+			} else if (dir.equals(directionTo(food))) {
+				// Speed up when going towards food
+				accelerateTo(2 * defaultSpeed, 0.3);
+			}
+		}
+	}
+
+	private void avoidRepellants() {
+		for (ISimObject obj : habitat.nearbyObjects(this, getRadius() + VIEW_DISTANCE)) {
+			if (obj instanceof SimRepellant && canSee(obj, VIEW_ANGLE) && habitat.contains(obj.getPosition())) {
+				opposite = directionTo(obj).turnBack();
+				
+				dir = dir.turnTowards(opposite, 1+4*(1-distanceTo(obj)/(VIEW_DISTANCE)));
+			}
+		}
+	}
+
 	@Override
 	public void step() {
 		// by default, move slightly towards center
@@ -86,34 +121,19 @@ public class SimAnimal extends AbstractMovingObject {
 		// go towards center if we're close to the border
 		if (!habitat.contains(getPosition(), getRadius() * 1.2)) {
 			dir = dir.turnTowards(directionTo(habitat.getCenter()), 5);
-			if (!habitat.contains(getPosition(), getRadius())) { 
+			if (!habitat.contains(getPosition(), getRadius())) {
 				// we're actually outside
 				accelerateTo(5 * defaultSpeed, 0.3);
 			}
 		}
-		
-		// If you find food, go towards it
-		IEdibleObject food = getBestFood();
-		
-		if (food != null){
-			dir = dir.turnTowards(directionTo(food), 2);
-			if (distanceToTouch(food) <=0){
-				// Stop moving when eating
-				energy += food.eat(0.5);
-				accelerateTo(0, 0.3);
-			}
-			else if (dir.equals(directionTo(food))){
-				// Speed up when going towards food
-				accelerateTo(2 * defaultSpeed, 0.3);
-			}
-		}
-		
 
+		goToFood();
+		avoidRepellants();
 
 		accelerateTo(defaultSpeed, 0.1);
-		
+
 		// Lose energy over time
-		if (energy < 0){
+		if (energy < 0) {
 			energy -= 0.05;
 		}
 
