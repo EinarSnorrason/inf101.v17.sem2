5bf3e4bc654c8ac59121cc7ab5b0e9b9ccdb5904
Einar Snorrason <Einar.Snorrason@student.uib.no>
2017-05-02 15:21:50 +0200

    Small changes to Pellet

--- src/inf101/simulator/objects/pacman/Pellet.java
+++ src/inf101/simulator/objects/pacman/Pellet.java
@@ -1,5 +1,7 @@
 package inf101.simulator.objects.pacman;
 
+import java.util.function.Consumer;
+
 import inf101.simulator.Direction;
 import inf101.simulator.Position;
 import inf101.simulator.objects.AbstractSimObject;
@@ -16,8 +18,14 @@ import javafx.scene.paint.Color;
 
 public class Pellet extends AbstractSimObject implements IEdibleObject{
 	
-	public static final int DIAMETER = 25;
-	public static final int POINTS = 10;
+	private static final int DIAMETER = 10;
+	private static final int POINTS = 10;
+	public static final Consumer<GraphicsContext> PAINTER = (GraphicsContext context) -> {
+		Pellet obj = new Pellet(new Position(0, 0));
+		obj.hideAnnotations = true;
+		context.scale(1 / obj.getWidth(), 1 / obj.getHeight());
+		obj.draw(context);
+	};
 
 	public Pellet(Position pos) {
 		super(new Direction(0), pos);
